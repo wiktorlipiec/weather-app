abbcfebf93838083afb489e5b096852036cb05eb
Wiktor Lipiec <wiktor@devanddeliver.com>
2022-06-27 21:55:04 +0200

    add air pollution

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 const addressSearch = document.querySelector('.address-search');
 const searchBox = new google.maps.places.SearchBox(addressSearch);
+const weatherForecast = document.querySelector('.weather-forecast');
+const airPollution = document.querySelector('.air-pollution');
 
 searchBox.addListener('places_changed', () => {
     const place = searchBox.getPlaces()[0];
@@ -9,6 +11,7 @@ searchBox.addListener('places_changed', () => {
     const latitude = place.geometry.location.lat();
     const longitude = place.geometry.location.lng();
 
+    cleanWeather();
     postData(latitude, longitude)
 });
 
@@ -25,13 +28,17 @@ function postData(latitude,longitude) {
         })
     }).then(res => res.json()).then(data => {
         console.log(data);
-        setWeatherData(data[0])
+        setWeatherData(data[0]);
+        setAirPollution(data[1]);
     })
 }
 
-function setWeatherData(data) {
-    const weatherForecast = document.querySelector('.weather-forecast');
+function cleanWeather() {
+    weatherForecast.innerText = '';
+    airPollution.innerText = '';
+}
 
+function setWeatherData(data) {
     //temperature
     const temperature = document.createElement('p');
     temperature.textContent = `Temperature: ${data.current.temperature}`;
@@ -41,4 +48,13 @@ function setWeatherData(data) {
     const img = document.createElement('img');
     img.src = data.current.weather_icons[0];
     weatherForecast.append(img);
+}
+
+function setAirPollution(data) {
+    const list = document.createElement('ul');
+
+    //Standard
+    data && data.current.standards.map(item => {
+        list.innerText += `<li>${item.percent}%</li>`
+    });
 }
